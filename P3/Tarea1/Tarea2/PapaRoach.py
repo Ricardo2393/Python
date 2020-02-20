b00a957c4c7bbbbbb07a7c102bab0c01b51f8429
Ricardo Tejera <ricardo.tejera23@gmail.com>
2020-02-19 20:43:34 -0500

    uupdate

--- P3/Tarea1/Tarea2/PapaRoach.py
+++ P3/Tarea1/Tarea2/PapaRoach.py
@@ -2,7 +2,7 @@ def pregunta():
     print ("Que quires hacer: ")
 
 def valor_invalido():
-    print("valor invalido ingrese otro valor que este entre 1 @ 3")
+    print("valor invalido ingrese otro valor que este entre 1 @ 3 o 9")
 
 def menu():
 
@@ -16,41 +16,55 @@ def menu():
 	print ("\t3. reproducir")
 
 	print ("\t9. Salir")
-ctotal = 10
-kill = 0   
-while True:
-    
-    
-    
-    menu()
-    
-    try:
-        pregunta()
-        opc = int(input(">>>>>>>>>:"))
-    except:
-        valor_invalido()
-        opc = int(input(">>>>>>>>>:"))
 
-    if opc ==1:
-        print ("con esta solo matas a una")
-        kill =+1
-        ctotal = ctotal - kill
-        print("cuantas quedan")
-        print(ctotal)
-    elif opc ==2:
-        print ("con esta solo matas a 5")
-        kill =+5
-        ctotal = ctotal - kill
-        print("cuantas quedan")
-        print(ctotal)
-    elif opc ==3:
-        print ("salieron 3 mas ")
-        ctotal = ctotal +3
-        print("cuantas quedan")
-        print(ctotal)
-    elif opc ==9:
-        break
+
+class Cucaracha:
+    def __init__(self, especie, subfilo, name, sci_name):
+        self.especie = "insecto"
+        self.subfilo = "invertebrado"
+        self.name = "Cucaracha"
+        self.sci_name = "Blatta"
+
+    def lore(self):
+        print("Tenemos un problema de " + self.name )
+c1 = Cucaracha("Bicho",)
+
+c1.lore()    
+
+# ctotal = 10
+# kill = 0   
+# while True:
     
     
-print("cuantas quedan")
-print(ctotal)
+    
+#     menu()
+    
+#     try:
+#         pregunta()
+#         opc = int(input(">>>>>>>>>:"))
+#     except:
+#         valor_invalido()
+#         opc = int(input(">>>>>>>>>:"))
+
+#     if opc ==1:
+#         print ("con esta solo matas a una")
+#         kill =+1
+#         ctotal = ctotal - kill
+#         print("cuantas quedan")
+#         print(ctotal)
+#     elif opc ==2:
+#         print ("con esta solo matas a 5")
+#         kill =+5
+#         ctotal = ctotal - kill
+#         print("cuantas quedan")
+#         print(ctotal)
+#     elif opc ==3:
+#         print ("salieron 3 mas ")
+#         ctotal = ctotal +3
+#         print("cuantas quedan")
+#         print(ctotal)
+#     elif opc ==9:
+#         break
+
+
+
